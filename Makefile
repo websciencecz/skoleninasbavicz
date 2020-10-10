4735d9844759e8d144738a3c062a464fae6dc1bb
jakubenglicky <jakubenglicky@gmail.com>
2020-10-10 22:40:24 +0200

    Přidání webinářů a livestream microsite

--- Makefile
+++ Makefile
@@ -3,6 +3,12 @@ build:
 		docker-compose run statie generate source -c statie_local.yml --output=html
 		rm output/css/style.css
 
+watch:
+		docker-compose run assets npx webpack --watch
+
+dev:
+		docker-compose run statie generate source -c statie_local.yml --output=html
+
 build-prod:
 		docker build -t jakubenglicky/skoleninasbavi-cz:latest .
 		docker push jakubenglicky/skoleninasbavi-cz:latest
--- source/js/main.js
+++ source/js/main.js
@@ -39,3 +39,32 @@ function topFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
+
+window.addEventListener('load', function (){
+	// Create an instance of the Stripe object with your publishable API key
+	var stripe = Stripe("pk_test_51HdBCNGg1BhZ64vx4A6PbkDfylmHDvnyXERjGs2sI7ZVliQ6tcHvcWQm6M597K0jvdkzv5BVfyx8n46ARtgLcpxy00wbR0XjM1");
+	var checkoutButton = document.getElementById("checkout-button");
+	checkoutButton.addEventListener("click", function () {
+		fetch("http://payments.skoleninasbavi.cz.test/create-session", {
+			method: "POST",
+		})
+			.then(function (response) {
+				return response.json();
+			})
+			.then(function (session) {
+				return stripe.redirectToCheckout({ sessionId: session.id });
+			})
+			.then(function (result) {
+				// If redirectToCheckout fails due to a browser or network
+				// error, you should display the localized error message to your
+				// customer using error.message.
+				if (result.error) {
+					alert(result.error.message);
+				}
+			})
+			.catch(function (error) {
+				console.error("Error:", error);
+			});
+	});
+});
+
