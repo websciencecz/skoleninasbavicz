f7919bd7bc87f090faf44e5a63fc8a6b2cc6ae2f
jakubenglicky <jakubenglicky@gmail.com>
2023-03-03 16:00:54 +0100

    Install Nuxt & First API requests

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="claim-box">
+            <img src="img/bg_hp.jpg" />
+        </div>
+        <div class="container-fluid content-box">
+            <div class="container">
+                <br>
+                <h2>Nejbližší veřejná školení:</h2>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    async asyncData({ $axios }) {
+        const response = await $axios.get('https://cms.skoleninasbavi.cz/api/snb-courses');
+        return {
+            courses: response.data
+        }
+    }
+}
+</script>
---
+++ nuxt/pages/lektori.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+      <div class="container-fluid content-box">
+        <div class="container">
+            <br>
+            <h1>Lektoři:</h1>
+            <br>
+            <div class="row">
+                <div class="col" v-for="lector in data" :key="lector.id">
+                   <div class="lector-box center">
+                        <a href="/lektori/jakub-englicky/">
+                            <img src="img/lectores/jakub-englicky.jpg?v={$assetsVersion}">
+                            <br>
+                            <div class="lector-name">
+                                <span class="bold">{{ lector.attributes.name }}</span>
+                            </div>
+                            <div class="tags">
+                                <span class="tag">Docker</span>
+                                <span class="tag">Git</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+        </div>
+	</div>
+</div>
+    </div>
+</template>
+
+<script>
+export default {
+    async asyncData({ params, error, $axios }) {
+        try {
+            const data = (await $axios.$get(`/api/snb-lectors`)).data;
+            return {
+                data,
+            };
+        } catch {
+            error({ statusCode: 404, message: 'No Page found' })
+        }
+    },
+}
+</script>
