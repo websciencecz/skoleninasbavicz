1bb3315db5795203c76c5e6ecdabe537b518478f
jakubenglicky <jakubenglicky@gmail.com>
2023-03-11 17:26:01 +0100

    Lector detail

--- nuxt/pages/lektori/_slug.vue
+++ nuxt/pages/lektori/_slug.vue
@@ -1,38 +1,39 @@
 <template>
-    <div>
+<div>
       <div class="container-fluid content-box">
         <div class="container">
-            <br>
-            <h1>Lektoři:</h1>
-            <br>
-            <div class="row">
-                <div class="col" v-for="lector in data" :key="lector.id">
-                   <div class="lector-box center">
-                        <a :href="`/lektori/${lector.attributes.slug}`">
-                            <img :src="lector.attributes.photo.data.attributes.url">
-                            <br>
-                            <div class="lector-name">
-                                <span class="bold">{{ lector.attributes.name }}</span>
-                            </div>
-                            <div class="tags">
-                                <span class="tag" v-for="tag in lector.attributes.tags" :key="tag.id">{{ tag.Tag }}</span>
-                            </div>
-                        </a>
-                    </div>
-                </div>
+           <br>
+			<h1>{{ lector.name}}</h1>
+			<hr>
+			<div class="about-lector">
+				<div class="lector-image">
+					<img :src="lector.photo.data.attributes.url" width="325">
+					<br>
+				</div>
+				<div class="lector-text">
+					<br>
+					<p>{{ lector.content}}</p>
+					<br>
+					<a v-for="social in lector.socials" :key="social.id" :href="social.link" target="_blank" class="social-icons-lector"><i :class="`${social.icon} fa-2x`"></i></a>
+				</div>
+			</div>
+			<br>
+			<h4>Moje školení</h4>
+			<hr>
+
         </div>
 	</div>
 </div>
-    </div>
+
 </template>
 
 <script>
 export default {
     async asyncData({ params, error, $axios }) {
         try {
-            const data = (await $axios.$get(`/api/snb-lectors/${params.slug}?populate=deep`)).data;
+            const lector = (await $axios.$get(`api/slugify/slugs/snb-lector/${params.slug}?populate=deep`)).data.attributes;
             return {
-                data,
+                lector,
             };
         } catch {
             error({ statusCode: 404, message: 'No Page found' })
